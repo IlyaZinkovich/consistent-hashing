ecec806f56f8522f462e2f37513ee5c21d13a76f
Ilya Zinkovich <ilya.zinkovich@gmail.com>
2020-09-03 00:25:04 +0200

    Add uniform servers load distribution

--- src/main/kotlin/consistent/hashing/App.kt
+++ src/main/kotlin/consistent/hashing/App.kt
@@ -15,15 +15,16 @@ object App {
         val ring = sortedMapOf<Hash, ServerIP>()
         val serverIPs = listOf("192.168.0.0", "192.168.0.1", "192.168.0.2")
         val hashFunction = Hashing.murmur3_32()
-        serverIPs.forEach { serverIP ->
-            val hash = hashFunction.hashString(serverIP, UTF_8).asInt()
+        val toHash = { str: String -> hashFunction.hashString(str, UTF_8).asInt() }
+        serverIPs.forEachIndexed { index, serverIP ->
+            val hash = Int.MIN_VALUE + index * (Int.MAX_VALUE / serverIPs.size - Int.MIN_VALUE / serverIPs.size)
             ring[hash] = serverIP
             println("server $serverIP has joined the ring with hash $hash")
         }
         val serverReceived = mutableMapOf<ServerIP, Int>()
         serverIPs.forEach { serverIP -> serverReceived[serverIP] = 0 }
         Stream.generate(UUID::randomUUID)
-            .map { hashFunction.hashString(it.toString(), UTF_8).asInt() }
+            .map { toHash(it.toString()) }
             .limit(1000000)
             .forEach { hash ->
                 val tail = ring.tailMap(hash)
